ef507c52a4322b51884d2111adcbdcf03f8024d4
LE Thomas <thomas.le.tle@gmail.com>
2024-05-04 19:21:32 +0200

    created required directories and files

---
+++ ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "BitcoinExchange.hpp"
+#include <vector>
+#include <deque>
+#include <list>
+
+int main()
+{
+    return (0);
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "PmergeMe.hpp"
+#include <list>
+
+int main()
+{
+    return (0);
+}
