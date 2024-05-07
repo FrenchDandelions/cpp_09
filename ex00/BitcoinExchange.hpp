1e892744ae285e8bba4719f72e888d8a96073c35
LE Thomas <thomas.le.tle@gmail.com>
2024-05-07 13:29:23 +0200

    class added

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,4 +1,30 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+#include <iostream>
+#include <map>
+
+class BTCExchange
+{
+    private:
+        BTCExchange();
+        std::map<std::string, double> _data;
+        std::map<std::string, double> _input;
+
+    public:
+        BTCExchange(const std::string &input_file);
+        BTCExchange(const BTCExchange &copy);
+        BTCExchange& operator=(const BTCExchange &copy);
+        ~BTCExchange();
+};
+
+BTCExchange::BTCExchange(/* args */)
+{
+}
+
+BTCExchange::~BTCExchange()
+{
+}
+
+
 #endif
